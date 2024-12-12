c99d05ef3bbc6dbad9c8abebd183f10bbe9d369c
Hanjit <hanbizmail@gmail.com>
2024-12-12 15:03:46 +0700

    Continue making popular activity

---
+++ app/src/main/java/com/example/letschill/PopularFragment.java
@@ -0,0 +1,25 @@
+package com.example.letschill;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PopularFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_popular, container, false);
+    }
+}
