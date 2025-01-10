ec6e3dce8f5b0fc6cba25c7d061ca87eed5e59b3
VronyX <160619847+VronyX@users.noreply.github.com>
2025-01-10 14:41:23 +0700

    Ganti API login

--- app/src/main/java/com/example/letschill/RegisterActivity.java
+++ app/src/main/java/com/example/letschill/RegisterActivity.java
@@ -10,11 +10,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,6 +30,8 @@ public class RegisterActivity extends AppCompatActivity {
     Button registerRegisterBtn;
     DatabaseReference database;
     TextView registerLoginTV;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +52,8 @@ public class RegisterActivity extends AppCompatActivity {
         registerLoginTV = findViewById(R.id.registerLoginTV);
 
         database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://letschill-01-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+        auth = FirebaseAuth.getInstance();
 
         registerLoginTV.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,23 +83,29 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }else{
 
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                               Toast.makeText(RegisterActivity.this, "Register Successful.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                               startActivity(intent);
+                               finish();
+                            }else {
+                                Toast.makeText(RegisterActivity.this, "Register Error.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                     database = FirebaseDatabase.getInstance().getReference("Users");
                     database.child(username).get().addOnCompleteListener(task -> {
                        if (task.isSuccessful()) {
                            if (task.getResult().exists()) {
                                Toast.makeText(RegisterActivity.this, "Username already exists", Toast.LENGTH_SHORT).show();
-                           }else {
+                           } else {
                                database.child(username).child("UserName").setValue(username);
                                database.child(username).child("Email").setValue(email);
                                database.child(username).child("Password").setValue(password);
-
-                               Toast.makeText(RegisterActivity.this, "Register Successful.", Toast.LENGTH_SHORT).show();
-                               Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                               startActivity(intent);
-                               finish();
                            }
-                       }else {
-                           Toast.makeText(RegisterActivity.this, "Error Login.", Toast.LENGTH_SHORT).show();
                        }
                     });
 
